7e37c4be72e0e626731244cd8af28b101e477eb0
Bluzzi <camilledugas.pro@gmail.com>
2022-10-29 16:21:03 +0200

    Use env variable instead config for bot token

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Goscord/Bot
 
 go 1.15
 
-require github.com/Goscord/goscord v0.0.0-20220826113633-6ebc522e133f
+require (
+	github.com/Goscord/goscord v0.0.0-20220826113633-6ebc522e133f
+	github.com/joho/godotenv v1.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"os"
+
 	"github.com/Goscord/Bot/command"
 	"github.com/Goscord/Bot/config"
 	"github.com/Goscord/Bot/event"
 	"github.com/Goscord/goscord"
 	"github.com/Goscord/goscord/gateway"
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -15,20 +18,30 @@ var (
 )
 
 func main() {
+	// Load envionment variables :
+	godotenv.Load()
+
 	Config, _ = config.GetConfig()
+
+	// Create client instance :
 	client = goscord.New(&gateway.Options{
-		Token:   Config.Token,
+		Token:   os.Getenv("BOT_TOKEN"),
 		Intents: gateway.IntentGuilds | gateway.IntentGuildMessages | gateway.IntentGuildMembers,
 	})
+
+	// Load command manager :
 	cmdMgr = command.NewCommandManager(client, Config)
 
+	// Load events :
 	_ = client.On("ready", event.OnReady(client, Config, cmdMgr))
 	_ = client.On("interactionCreate", cmdMgr.Handler(client, Config))
 	_ = client.On("guildMemberAdd", event.OnGuildMemberAdd(client, Config))
 
+	// Login client :
 	if err := client.Login(); err != nil {
 		panic(err)
 	}
 
+	// Keep bot running :
 	select {}
 }
